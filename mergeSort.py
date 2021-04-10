0a699e630d14943f9bce58dde17dcafd63b12b93
clee <clee@tuta.io>
2021-04-10 01:24:43 +0000

    add mergeSort

---
+++ mergeSort.py
@@ -0,0 +1,58 @@
+import math
+
+class MergeSort:
+
+    def __init__(self, items):
+        self.recursiveCalls = 0
+        self.items = items
+
+    #Splitting string into equal halves
+    def splitArray(self,s):
+        return s[:len(s)//2], s[len(s)//2:]
+
+    def solve(self):
+        return self.solveRecursive(self.items)
+
+    # the idea here is i+j should sum to n as you increment i and j, 
+    # and once out of bound, the next item a or b is infinity
+    def merge(self, a, b, n):
+        result = [0] * n
+        a = a + [math.inf]
+        b = b + [math.inf]
+        result = [0] * n
+        i, j = 0, 0
+        for k in range(0, n):
+            if a[i] < b[j]:
+                result[k] = a[i]
+                i+=1
+            else:
+                result[k] = b[j]
+                j+=1
+        return result
+
+    def solveRecursive(self, items):
+        self.recursiveCalls += 1
+        n = len(items)
+        baseCase = []
+        if n == 0:
+            return baseCase
+        if n == 1:
+            baseCase.append(items[0])
+            return baseCase
+        if n == 2:
+            if items[0] < items[1]:
+                baseCase.append(items[0])
+                baseCase.append(items[1])
+                return baseCase
+            else:
+                baseCase.append(items[1])
+                baseCase.append(items[0])
+                return baseCase
+        left, right = self.splitArray(items)
+        sortedLeft = self.solveRecursive(left)
+        sortedRight = self.solveRecursive(right)
+        return self.merge(sortedLeft,sortedRight,n)
+    
+    def printRecursiveCalls(self):
+        print('MergeSort', self.recursiveCalls)
+
--- test.py
+++ test.py
@@ -1,15 +1,16 @@
+
 def testCoinchange():
-    import coinchange
+    import coinChange
     coins = [2, 5, 3, 7, 9]
     capacity = 1000
-    c = coinchange.CoinChange()
+    c = coinChange.CoinChange()
     result = c.solve(coins, capacity)
     print(result, 'combinations')
 
 
 def testRecIntMult(a, b):
-    import recintmult
-    r = recintmult.RecIntMult(a,b)
+    import recIntMult
+    r = recIntMult.RecIntMult(a,b)
     result = r.solve()
     print(result)
     r.printRecursiveCalls()
@@ -21,6 +22,24 @@ def testKaratsuba(a, b):
     print(result)
     r.printRecursiveCalls()
 
-a, b = 1234, 5678
-testRecIntMult(a, b)
-testKaratsuba(a, b)
+def testMergeSort():
+    import mergeSort
+    import random
+    a = [random.randrange(1, 100, 1) for i in range(100)]
+    print(len(a))
+    tA = mergeSort.MergeSort(a)
+    testA = tA.solve()
+    
+    from sorting_techniques import pysort
+    sortObj = pysort.Sorting()
+    testB = sortObj.mergeSort(a)
+    if len(testA) != len(testB):
+        print('array not equal', len(testA), len(testB))
+        return
+    for i in range(len(a)):
+        print(testA[i], testB[i])
+        if (testA[i]) != (testB[i]):
+            print("error")
+    tA.printRecursiveCalls()
+    
+testMergeSort()
