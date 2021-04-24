9157dbbc9a85c42942c7bff059a55845267b456e
clee <clee@tuta.io>
2021-04-24 04:00:12 -0700

    return points (not just closest)

--- closestDistance.py
+++ closestDistance.py
@@ -19,104 +19,113 @@ class ClosestDistance:
         self.B = sorted(items, key=lambda point: point.y)
 
     def solve(self):
-        return self.solveRecursive(self.A, self.B)
+        return self.solveRecursive(self.A, self.B, len(self.A))
 
-    def splitArray(self,arrA):
-        return arrA[:len(arrA)//2], arrA[len(arrA)//2:]
-
-    def solveRecursive(self, arrA, arrB):
-        self.recursiveCalls += 1
-        n = len(arrA)
-        if n == 0:
-            raise Exception('n=0')
-        # If there are 2 or 3 points, 
-        # then use brute force 
-        if n <= 3:
-            min_val = self.bruteForce(arrA, n)
-            #baseCaseR, min_r = self.bruteForce(arrB, n)
-            return arrA, min_val
-
-        mid = n // 2
-        midPoint = arrA[mid]
-        leftA, rightA = self.splitArray(arrA)
-        dividedLeftA, distLeftA = self.solveRecursive(leftA, arrB)
-        dividiedRightA, distRightA = self.solveRecursive(rightA, arrB)
-        mergedA, merge_min = self.merge(dividedLeftA,dividiedRightA, distLeftA, distRightA, n, midPoint, arrB)
-        return mergedA, merge_min
-
-    def merge(self, a, b, distLeftA, distRightA, n, midPoint, Q):
-        lr_d = min(distLeftA, distRightA)
-
-        m = n//2
-        inversionCount = 0
-        result = [0] * n
-        a = a + [Point(math.inf,math.inf)]
-        b = b + [Point(math.inf,math.inf)]
-        i, j = 0, 0
-        stripA = []
-        stripB = []
-        for k in range(0, n):
-            if a[i].x < b[j].x:
-                result[k] = a[i]
-                i+=1
-            else:
-                result[k] = b[j]
-                inversionCount += n//2 - i
-                if inversionCount > 0:
-                    raise Exception('inversionCount increased. List is not sorted')
-                j+=1
-
-            if abs(result[k].x - midPoint.x) < lr_d: 
-                stripA.append(result[k])
-            if abs(Q[k].x - midPoint.x) < lr_d: 
-                stripB.append(Q[k])
+    def splitArray(self,arrA,arrB):
+        m = len(arrA)//2
+        return arrA[:m], arrB[:m], arrA[m:], arrB[m:]
         
-        #REQUIRED 
-        # because: while j < size and (dist := self.dist(strip[j],strip[i])) < min_val
-        # meaning this will stop the moment it finds the first y value that is less than
-        # min_val (or it gets to the end of the list). See stripClosest for more info
-        stripA.sort(key = lambda point: point.y) #<-- REQUIRED
-
-        min_a = self.stripClosest(stripA, len(stripA), lr_d)
-        min_b = self.stripClosest(stripB, len(stripB), lr_d)
-        
-        return result, min(min_a, min_b)
-
     # A Brute Force method to return the
     # smallest distance between two points
     # in P[] of size n
     def bruteForce(self, P, n, min_val=float('inf')):
+        best = []
         for i in range(n):
             for j in range(i + 1, n):
                 min_distance = self.dist(P[i], P[j])
                 if min_distance < min_val:
                     min_val = self.dist(P[i], P[j])
-        return min_val
-    
-    # A utility function to find the 
-    # distance beween the closest points of 
-    # strip of given size. All points in 
-    # strip[] are sorted accordint to 
-    # y coordinate. They all have an upper 
-    # bound on minimum distance as d. 
-    # Note that this method seems to be 
-    # a O(n^2) method, but it's a O(n) 
-    # method as the inner loop runs at most 6 times
-    # IMPORTANT: Preprocessing Ruequired: strip must be sorted by 'y' point asc
-    def stripClosest(self, strip, size, d):
+                    best = [P[i], P[j]]
+        return best, min_val
+
+    def solveRecursive(self, x, y, n):
+        self.recursiveCalls += 1
+        n = len(x)
+        if n == 0:
+            raise Exception('n=0')
+        # If there are 2 or 3 points, 
+        # then use brute force 
+        if n <= 3:
+            return self.bruteForce(x, n)
+
+        mid = n//2
+        #midPoint = x[mid-1]
+
+        Lx, Ly, Rx, Ry = self.splitArray(x,y)
+        l12, bestL = self.solveRecursive(Lx, Ly, mid)
+        r12, bestR = self.solveRecursive(Rx, Ry, n - mid)
+        bestP = []
+        best = math.inf
+        if bestL < bestR:
+            bestP = l12
+            best = bestL
+        else:
+            bestP = r12
+            best = bestR
+        s12, bestS = self.merge(Lx, Rx, Ly, Ry, best, n)
+        if bestS < best:
+            bestP = s12
+            best = bestS
+        return bestP, best
+    def merge(self, Lx, Rx, Ly, Ry, lr_d, n):
+        midPoint = Lx[len(Lx)-1]
+        Sx = []
+        Px = [0] * n
+        i, j = 0, 0
+        Lx = Lx + [Point(math.inf,math.inf)]
+        Rx = Rx + [Point(math.inf,math.inf)]
+        for k in range(0, n):
+            if Lx[i].x < Rx[j].x:
+                Px[k] = Lx[i]
+                i+=1
+            else:
+                Px[k] = Rx[j]
+                j+=1
         
-        # Initialize the minimum distance as d 
-        min_val = d 
+            if abs(Px[k].x - midPoint.x) < lr_d: 
+                Sx.append(Px[k])
+
+        Sx.sort(key = lambda point: point.y) #<-- REQUIRED
+
+        bestL, lr_d2 = self.stripClosest(Sx, len(Sx), [], lr_d)
+
+        Sy = []
+        Py = [0] * n
+        Ly = Ly + [Point(math.inf,math.inf)]
+        Ry = Ry + [Point(math.inf,math.inf)]
+        i, j = 0, 0
+        
+        leftside = abs(midPoint.x - lr_d)
+        for k in range(0, n):
+            if Ly[i].y < Ry[j].y:
+                Py[k] = Ly[i]
+                i+=1
+            else:
+                Py[k] = Ry[j]
+                j+=1
+            if abs(Py[k].x - midPoint.x) < leftside: 
+                Sy.append(Py[k])
+        best, lr_d = self.stripClosest(Sy, len(Sy), bestL, lr_d)
+
+        if lr_d2 < lr_d:
+            best = bestL
+            lr_d = lr_d2
+        return best, lr_d
     
-        # Pick all points one by one and 
-        # try the next points till the difference 
-        # between y coordinates is smaller than d. 
-        # This is a proven fact that this loop
-        # runs at most 6 times 
+    def stripClosest(self, strip, size, best, min_dis):
+        '''
+        for i in range(min(6, size - 1), size):
+            for j in range(max(0, i - 6), i):
+                current_dis = self.dist(strip[i],strip[j])
+                if current_dis < min_dis:
+                    min_dis = current_dis
+                    best = [strip[i], strip[j]]
+        return best, min_dis
+        '''
         for i in range(size):
             j = i + 1
-            while j < size and (dist := self.dist(strip[j],strip[i])) < min_val: 
-                min_val = dist
+            while j < size and (dist := self.dist(strip[i],strip[j])) < min_dis: 
+                min_dis = dist
+                best = [strip[i], strip[j]]
                 j += 1
-    
-        return min_val 
+        return best, min_dis
